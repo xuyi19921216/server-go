7c87de91c1d008bc6e32259d73085835db9d802a
killianxu <killianxu@tencent.com>
2024-11-21 22:20:26 +0800

    增加第3节参考答案

--- 3/main_test.go
+++ 3/main_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-var slices []int
+/* var slices []int
 
 func init() {
 	slices = getRawSlices()
-}
+} */
 
 /*
 	 func BenchmarkGetRawSet(b *testing.B) {
@@ -17,7 +17,7 @@ func init() {
 		}
 	}
 */
-func BenchmarkGetEmptyStructSet(b *testing.B) {
+/* func BenchmarkGetEmptyStructSet(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		getEmptyStructSet(slices)
 	}
@@ -27,7 +27,7 @@ func BenchmarkGetCapacitySet(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		getCapacitySet(slices)
 	}
-}
+} */
 
 /* func BenchmarkGetRawSlices(b *testing.B) {
 	for n := 0; n < b.N; n++ {
@@ -40,3 +40,44 @@ func BenchmarkGetCapacitySlices(b *testing.B) {
 		getCapacitySlices()
 	}
 } */
+
+type Item struct {
+	id  int
+	val [2048]byte
+}
+
+// 用下标遍历[]struct{}
+func BenchmarkSliceStructByFor(b *testing.B) {
+	var items [2048]Item
+	for i := 0; i < b.N; i++ {
+		var tmp int
+		for j := 0; j < len(items); j++ {
+			tmp = items[j].id
+		}
+		_ = tmp
+	}
+}
+
+// 用range的下标遍历[]struct{}
+func BenchmarkSliceStructByRangeIndex(b *testing.B) {
+	var items [2048]Item
+	for i := 0; i < b.N; i++ {
+		var tmp int
+		for k := range items {
+			tmp = items[k].id
+		}
+		_ = tmp
+	}
+}
+
+// 用range遍历[]struct{}的元素
+func BenchmarkSliceStructByRangeValue(b *testing.B) {
+	var items [2048]Item
+	for i := 0; i < b.N; i++ {
+		var tmp int
+		for _, item := range items {
+			tmp = item.id
+		}
+		_ = tmp
+	}
+}
